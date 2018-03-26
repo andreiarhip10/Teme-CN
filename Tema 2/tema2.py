766c7b70263b0cc87b85271ae171a6a02874f655
sabisav <sabisav@yahoo.com>
2018-03-26 15:05:31 +0300

    check solution

--- Tema 2/tema2.py
+++ Tema 2/tema2.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.linalg import inv
 from random import randint
+import math
 
 def random_system_matrix():
     n = randint(2, 5)
@@ -15,25 +16,42 @@ def random_system_matrix():
         line = []
     return a, b
 #print (random_system_matrix()[1])
-#ainit=np.array([[0,0,4,],[0,2,6],[2,4,3]])
-#binit=np.array([2,3,1])
+# ainit=np.array([[0,0,4,],[0,2,6],[2,4,3]])
+# binit=np.array([2,3,1])
 (ainit, binit) = random_system_matrix()
 a=ainit
 b=binit
 n=len(a)
+xgauss = np.zeros((n,), dtype=float)
+
+m=2
+#m = int(input("Precizia, m"))
+eps=pow(10,-m)
+
+print("Matricea A: ")
+for i in range(0,n):
+    print(ainit[i])
+print("Matricea B: ", binit)
 
 #rezolvare directa
-x = np.zeros((n,), dtype=float)
-x = np.linalg.solve(a, b)
-print(x)
+xlib = np.zeros((n,), dtype=float)
+if np.linalg.det(ainit)!=0:
+     print("Rezultat librarie: ")
+     xlib=(np.dot(inv(ainit),binit))
+     print(xlib)
+else:
+     print("Nu are solutie")
+
 
 #verificare solutie
 def verifica_solutie(x):
     print(x)
+    print("Rezultat verificare: ")
+    print(np.dot(ainit,x)-binit)
 
 # metoda substitutiei
-def rez_sistem(a,b,n):
-    x = np.zeros((n,), dtype=float)
+def rez_sistem(a,b,n,x):
+
     x[n - 1] = b[n - 1] / a[n - 1][n - 1]
     for i in range(n - 2, -1, -1):
         x[i] = b[i]
@@ -64,7 +82,7 @@ def algoritm(a,b,n):
     l=0
 
     #dat de la tastatura
-    eps=0
+    #eps=0
     while(l<n-1 and a[l][l]>eps):
 
         ##gauss 5,6,7
@@ -97,9 +115,20 @@ def algoritm(a,b,n):
     if a[l][l]<=eps:
         print("Matrice singulara")
     else:
-        rez_sistem(a,b,n)
-        verifica_solutie(x)
+        # print("Matricea A transformata: ")
+        # for i in range(0,n):
+        #     print(a[i])
+        rez_sistem(a,b,n,xgauss)
+        verifica_solutie(xgauss)
 
+print("Rezultat Gauss: ")
 algoritm(a,b,n)
 
-
+no1=[]
+no2=[]
+print("Norme: ")
+inva=inv(ainit)
+for i in range(0,n):
+    no1.append(math.sqrt(pow(xgauss[i],2)+pow(xlib[i],2)-2*xgauss[i]*xlib[i]))
+print(no1)
+print(no2)
